0b4938c79958ed09486dfe095aeb43a6c7c2fbda
Jesus Rumie Vittar <23528273+jrumievittar@users.noreply.github.com>
2023-06-14 19:23:26 +0200

    script file main.go

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
+package main
 
+import "fmt"
+
+func suma(x int, y int) int {
+	return x + y
+}
+
+func resta(x int, y int) int {
+    return x - y
+}
+
+func main() {
+	fmt.Println(suma(4, 3))
+	fmt.Println(resta(8, 2))
+}
